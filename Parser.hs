eca451ad472aa6b6f42801b194b416fd4bf16ee9
nathan-08 <nathanryan001@gmail.com>
2022-07-31 01:53:17 -0600

    beta reduction seems to be working

--- Parser.hs
+++ Parser.hs
@@ -10,9 +10,9 @@ data Node = NameNode [Char]
 -- parse list of Lexemes into AST
 expr ((Name name):xs) = (Just (Expression (NameNode name)), xs)
 expr (lexms) =
-    let (func, rest) = function(lexms)
-    in case func of
-      Just f -> (Just (Expression f), rest)
+    let (maybe_func, rest) = function(lexms)
+    in case maybe_func of
+      Just func -> (Just (Expression func), rest)
       Nothing -> 
         let (maybe_app, rest) = application(lexms)
         in case maybe_app of
@@ -40,4 +40,36 @@ application (LParen:xs) =
 application _ = (Nothing, [])
 
 
+apply parm arg body =
+  --replace instances of NameNode parm in body with arg
+  case body of
+    Expression(NameNode name) -> if name == parm then arg else body
 
+    Expression(Function p b) ->
+      Expression (Function p (apply parm arg b))
+
+    Expression(Application (Expression (Function p' fbod)) abod) ->
+      beta_reduce (Expression(
+          Application
+            (Expression (Function p' (apply parm arg fbod)))
+            (apply parm arg abod)))
+
+    Expression(Application e1 e2) ->
+      beta_reduce (Expression(
+            Application
+            (apply parm arg e1)
+            (apply parm arg e2)
+            ))
+
+--beta reduction
+--traverse tree top down to find and eliminate application nodes
+beta_reduce :: Node -> Node
+beta_reduce (Expression
+    (Application (Expression (Function (NameNode parm) body)) arg)) =
+  case arg of
+    (Expression (Application _ _) ) ->
+      (apply parm (beta_reduce (Expression arg)) body)
+    (Expression _) -> (apply parm arg body)
+
+beta_reduce x = x
+  
