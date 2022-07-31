efe95f715f20805d0ac8bcc070e81995ab28e7ff
nathan-08 <nathanryan001@gmail.com>
2022-07-30 22:23:29 -0600

    fix bugs and add grammar specification

--- lexer.hs
+++ lexer.hs
@@ -1,4 +1,4 @@
--- λ
+--λ 
 readdat = readFile "input.dat"
 
 rstrip str = 
@@ -19,13 +19,15 @@ data Lexeme = Lambda
 split_word [] = ([], [])
 split_word str =
   let f [] a = a
-      f ('.':xs) (a, b) = (a, '.':xs)
+      f ('.':xs) (a, _) = (a, '.':xs)
+      f (')':xs) (a, _) = (a, ')':xs)
+      f (' ':xs) (a, _) = (a, ' ':xs)
       f (c:xs) (a, b) = f xs (c:a, b)
       (a, b) = f str ([], [])
   in (reverse a, b)
 
 lexfn [] = []
-lexfn ('L':xs) = Lambda : (lexfn xs)
+lexfn ('λ':xs) = Lambda : (lexfn xs)
 lexfn ('.':xs) = Dot    : (lexfn xs)
 lexfn ('(':xs) = LParen : (lexfn xs)
 lexfn (')':xs) = RParen : (lexfn xs)
