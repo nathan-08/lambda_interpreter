130c62be373b2284164cf336e16599492ae04d79
nathan-08 <nathanryan001@gmail.com>
2022-07-31 11:00:02 -0600

    refactor parser; create main

--- Parser.hs
+++ Parser.hs
@@ -1,75 +1,76 @@
+module Parser
+( parse
+, beta_reduce
+) where
 import Lexer
 
-data Node = NameNode [Char]  
-          | Function Node Node
-          | Expression Node
-          | Application Node Node
-          | FnExpression Node
-          | ArgExpression Node
-          deriving (Show)
--- parse list of Lexemes into AST
-expr ((Name name):xs) = (Just (Expression (NameNode name)), xs)
-expr (lexms) =
-    let (maybe_func, rest) = function(lexms)
-    in case maybe_func of
-      Just func -> (Just (Expression func), rest)
-      Nothing -> 
-        let (maybe_app, rest) = application(lexms)
-        in case maybe_app of
-          Just app -> (Just (Expression app), rest)
-          Nothing -> (Nothing, [])
+data Expression = NameExpr [Char]
+                | Function [Char] Expression
+                | Application Expression Expression
+                deriving (Show)
 
+parse (lxms) = fst (expression lxms)
+-- AST building functions
+-- <expression> = <name> | <function> | <application>
+expression ((Name name):xs) = (Just (NameExpr name), xs)
+expression (lexms) =
+  let (maybe_func, rest) = function lexms
+  in case maybe_func of
+    Just func -> (maybe_func, rest)
+    Nothing   ->
+      let (maybe_app, rest) = application lexms
+      in case maybe_app of
+        Just app -> (maybe_app, rest)
+        Nothing -> (Nothing, [])
+
+-- <function> = λ<name>.<expression>
 function (Lambda:(Name name):Dot:xs) =
-  let (maybe_exp, rest) = expr(xs)
+  let (maybe_exp, rest) = expression xs
   in case maybe_exp of
-    Just exp -> (Just (Function (NameNode name) exp), rest)
+    Just exp -> (Just (Function name exp), rest)
     Nothing -> (Nothing, [])
 function _ = (Nothing, [])
 
-application (LParen:xs) = 
-  let (maybe_fexpr, rest) = expr(xs)
+-- <application> = (<function expresson> <argument expression>)
+application (LParen:xs) =
+  let (maybe_fexpr, rest) = expression xs
+      maybe_space = head rest
+      (maybe_argexpr, rest') = expression (tail rest)
+      maybe_RParen = head rest'
+      rest'' = tail rest'
   in case maybe_fexpr of
     Nothing -> (Nothing, [])
-    Just fexpr -> 
-      let (maybe_argexpr, rest') = expr(tail rest)
-      in case maybe_argexpr of
+    Just fexpr ->
+      case maybe_argexpr of
         Nothing -> (Nothing, [])
-        Just argexpr -> if isSpace (head rest)
-                        then (Just (Application fexpr argexpr), tail rest')
-                        else (Nothing, [])
+        Just argexpr ->
+          if isSpace maybe_space && isRParen maybe_RParen
+          then (Just (Application fexpr argexpr), rest'')
+          else (Nothing, [])
+
 application _ = (Nothing, [])
 
-
-apply parm arg body =
-  --replace instances of NameNode parm in body with arg
-  case body of
-    Expression(NameNode name) -> if name == parm then arg else body
-
-    Expression(Function p b) ->
-      Expression (Function p (apply parm arg b))
-
-    Expression(Application (Expression (Function p' fbod)) abod) ->
-      beta_reduce (Expression(
-          Application
-            (Expression (Function p' (apply parm arg fbod)))
-            (apply parm arg abod)))
-
-    Expression(Application e1 e2) ->
-      beta_reduce (Expression(
-            Application
-            (apply parm arg e1)
-            (apply parm arg e2)
-            ))
-
 --beta reduction
---traverse tree top down to find and eliminate application nodes
-beta_reduce :: Node -> Node
-beta_reduce (Expression
-    (Application (Expression (Function (NameNode parm) body)) arg)) =
+--find and eliminate Application expressions
+beta_reduce :: Expression -> Expression
+beta_reduce (Application (Function parm body) arg) =
   case arg of
-    (Expression (Application _ _) ) ->
-      (apply parm (beta_reduce (Expression arg)) body)
-    (Expression _) -> (apply parm arg body)
+    Application _ _ -> apply parm (beta_reduce arg) body
+    _ -> apply parm arg body
 
 beta_reduce x = x
   
+apply :: String -> Expression -> Expression -> Expression
+apply parm arg body =
+  --replace instances of NameExpr parm in body with arg
+  case body of
+    NameExpr name -> if name == parm then arg else body
+
+    Function p b ->
+      Function p (apply parm arg b)
+
+    Application e1 e2 ->
+      beta_reduce (Application
+            (apply parm arg e1)
+            (apply parm arg e2))
+
---
+++ main.hs
@@ -0,0 +1,16 @@
+import Lexer
+import Parser
+
+main :: IO ()
+main = do
+  t <- readdat
+  let t' = rstrip t
+      lexms = lexfn t'
+      maybe_ast = parse lexms
+      reduced = case maybe_ast of
+        Just ast -> Just (beta_reduce ast)
+        Nothing -> Nothing
+  case reduced of
+    Just r -> print r
+    Nothing -> print "something went wrong"
+
